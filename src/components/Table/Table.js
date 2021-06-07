ce1002844baa7d0baeff57d3f8b4daefd11941f4
maramelsi <32310256+maramelsi@users.noreply.github.com>
2021-06-07 05:21:05 +0300

    Used react-bootstrap

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import TableBody from './TableBody';
 import '../css/table.css';
+import {Container} from 'react-bootstrap';
 
 const Table = (props) => {
   const { data } = props;
 
   return (
-    <div className="container">
-      <div className="row">
-      <div className="col-md-3"> <TableBody list={data}/></div>
-        <div className="col-md-3"> <TableBody list={data}/></div>
-        <div className="col-md-3"> <TableBody list={data}/></div>
-        <div className="col-md-3"> <TableBody list={data}/></div>
-      </div>
-    </div>
+    <Container>
+      <TableBody list={data}/>
+    </Container>
   );
 }
 
--- src/components/Table/TableBody.js
+++ src/components/Table/TableBody.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import Thumbnail from '../Thumbnail';
+/*import Thumbnail from '../Thumbnail';*/
 import images from '../../images/index';
+import {Row, Col, Card, Button} from 'react-bootstrap';
 
 const TableBody = (props) => {
   const list = props.list;
   
   return (
-    <tbody>
+    <Row>
       {list.map((item, index) => {
         return (
-          <div className="card" style={{width: "18rem"}} key={`movie-${index}`}>
-            <img className="card-img-top" src={images[item.img]} alt={item.alt} />
-            <div className="card-body">
-              <h5 className="card-title">{item.Title}</h5>
-              <h6 className="card-subtitle mb-2 text-muted">{item.Year} | {item.Genre} | {item.Language} | {item.Runtime}</h6>
-              <p className="card-text">{item.Plot}</p>
-              <a href="#" class="btn btn-primary">Goto movie</a>
-            </div>
-          </div>
+          <Col>
+          <Card style={{width: "18rem"}} key={`movie-${index}`}>
+           <Card.Img variant="top" src={images[item.img]} alt={item.alt} />
+            <Card.Body>
+              <Card.Title>{item.Title}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">{item.Year} | {item.Genre} | {item.Language} | {item.Runtime}</Card.Subtitle>
+              <Card.Text>{item.Plot}</Card.Text>
+              <Button variant="primary">Goto movie</Button>
+            </Card.Body>
+          </Card>
+          </Col>
         );
       })}
-    </tbody>);
+    </Row>);
 }
 
 export default TableBody
