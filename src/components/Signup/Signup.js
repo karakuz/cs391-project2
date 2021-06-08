45927b043309636046af6c60538ee06e1876cfc4
DoguhanB <73018150+DoguhanB@users.noreply.github.com>
2021-06-08 19:01:17 +0300

    handleSubmit is missing

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,54 +1,77 @@
-import React from 'react'
+import React, { Component } from "react";
 import { Col, Row, Form, Button } from 'react-bootstrap';
 import '../css/login.css'
 
-const Signup = () => {
-  return (
-    <div className="formDiv">
-      <Form>
-        
-        <Form.Group as={Row} controlId="formPlaintextEmail">
-          <Form.Label column sm="2">
-            Email
-          </Form.Label>
-          <Col sm="10">
-            <Form.Control type="email" placeholder="Email" />
-          </Col>
-        </Form.Group>
+class Signup extends Component {
+  constructor(props) {
+    super(props);
 
-        <Form.Group as={Row} controlId="formPlaintextPassword">
-          <Form.Label column sm="2">
-            Password
-          </Form.Label>
-          <Col sm="10">
-            <Form.Control type="password" placeholder="Password" />
-          </Col>
-        </Form.Group>
-        <Form.Group as={Row} controlId="formPlaintextPassword">
-          <Form.Label column sm="2">
-            Password(*)
-          </Form.Label>
-          <Col sm="10">
-            <Form.Control type="password" placeholder="Password" />
-          </Col>
-        </Form.Group>
-        <Form.Group as={Row}>
-          <Col>
-            <Form.Control placeholder="First name" />
-          </Col>
-          <Col>
-            <Form.Control placeholder="Last name" />
-          </Col>
-        </Form.Group>
-        <div className="buttonDiv">
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Remember me" />
+    this.state = {
+      email: "",
+      password: "",
+      password_confirmation: "",
+      name: "",
+      surname: "",
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+    handleChange(event) {
+      this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit(event) {
+
+    }
+
+  render() {
+    return (
+      <div className="formDiv">
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="2">
+              Email
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} required />
+            </Col>
           </Form.Group>
-          <Button>Log in</Button>
-        </div>
-      </Form>
-    </div>
-  )
+          <Form.Group as={Row} controlId="formPlaintextPassword">
+            <Form.Label column sm="2">
+              Password
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.handleChange} required />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row} controlId="formPlaintextPassword">
+            <Form.Label column sm="2">
+              Password(*)
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control type="password" placeholder="Confirm Password" name="password_confirmation" value={this.state.password_confirmation} onChange={this.handleChange} required />
+            </Col>
+          </Form.Group>
+          <Form.Group as={Row}>
+            <Col>
+              <Form.Control placeholder="First name" name="name" value={this.state.name} onChange={this.handleChange} required />
+            </Col>
+            <Col>
+              <Form.Control placeholder="Last name" name="surname" value={this.state.surname} onChange={this.handleChange} required />
+            </Col>
+          </Form.Group>
+          <div className="buttonDiv">
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Remember me" />
+            </Form.Group>
+            <Button type="submit">Sign Up</Button>
+          </div>
+        </Form>
+      </div>
+    )
+  };
 }
 
 export default Signup
