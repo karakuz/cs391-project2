5f2169c4f143c58e35b772c0e03ac74d60b9ec78
DoguhanB <73018150+DoguhanB@users.noreply.github.com>
2021-06-08 19:00:40 +0300

    handleSubmit is missing

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,38 +1,58 @@
-import React from 'react'
+import React, { Component } from "react";
 import { Col, Row, Form, Button } from 'react-bootstrap';
 import '../css/login.css'
 
-const Login = () => {
-  return (
-    <div className="formDiv">
-      <Form>
-        <Form.Group as={Row} controlId="formPlaintextEmail">
-          <Form.Label column sm="2">
-            Email
-          </Form.Label>
-          <Col sm="10">
-            <Form.Control type="email" placeholder="Email" />
-          </Col>
-        </Form.Group>
+class Login extends Component {
+  constructor(props) {
+    super(props);
 
-        <Form.Group as={Row} controlId="formPlaintextPassword">
-          <Form.Label column sm="2">
-            Password
-          </Form.Label>
-          <Col sm="10">
-            <Form.Control type="password" placeholder="Password" />
-          </Col>
-        </Form.Group>
-        
-        <div className="buttonDiv">
-          <Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Remember me" />
+    this.state = {
+      email: "",
+      password: "",
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+    handleChange(event) {
+      this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit(event) {
+
+    }
+
+  render() {
+    return (
+      <div className="formDiv">
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group as={Row} controlId="formPlaintextEmail">
+            <Form.Label column sm="2">
+              Email
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} required />
+            </Col>
           </Form.Group>
-          <Button>Log in</Button>
-        </div>
-      </Form>
-    </div>
-  )
+          <Form.Group as={Row} controlId="formPlaintextPassword">
+            <Form.Label column sm="2">
+              Password
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.handleChange} required />
+            </Col>
+          </Form.Group>
+          <div className="buttonDiv">
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Remember me" />
+            </Form.Group>
+            <Button type="submit">Log In</Button>
+          </div>
+        </Form>
+      </div>
+    )
+  };
 }
 
 export default Login
